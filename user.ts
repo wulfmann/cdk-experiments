8afd6f77a9d8b01ed498c1523bb2e0b55bad1a98
Joseph Snell <joepsnell@gmail.com>
2020-05-07 22:27:17 -0500

    Update user.ts

--- user.ts
+++ user.ts
@@ -17,11 +17,7 @@ const key = new iam.CfnAccessKey(stack, 'AccessKey', {
   status: 'Active'
 });
 
-const secretKey = cdk.Lazy.stringValue({
-  produce: () => {
-    return cdk.Fn.getAtt('AccessKey', 'SecretAccessKey')
-  }
-});
+const secretKey = cdk.Fn.getAtt('AccessKey', 'SecretAccessKey');
 
 const userSecret = JSON.stringify({
   accessKey: key.ref,
