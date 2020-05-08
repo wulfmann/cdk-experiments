655c979738bbf0500ed2ff31819ff72a1f5eba6a
Joseph Snell <joepsnell@gmail.com>
2020-05-07 21:24:30 -0500

    Update user.ts

--- user.ts
+++ user.ts
@@ -1,5 +1,6 @@
 import * as cdk from '@aws-cdk/core'
 import * as iam from '@aws-cdk/aws-iam'
+import * as secrets from '@aws-cdk/aws-secretsmanager'
 
 // CDK Configuration
 const app = new cdk.App();
@@ -7,3 +8,13 @@ const stack = new cdk.Stack(app, 'Stack');
 
 // Resources
 const user = new iam.User(stack, 'User');
+
+
+const userSecret = JSON.Stringify({
+  accessKey: user.ref,
+  secretKey: cdk.Fn.getAtt(user, 'SecretKey')
+});
+
+new secrets.CfnSecret(stack, 'Secret', {
+  SecretString: userSecret
+});
